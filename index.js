cee2b35ef0f0386f5051aa0d4e22c18f2e6f9b96
Mitchell <Hmitchell.hall@gmail.com>
2021-08-16 10:34:41 -0500

    commit tree cf32f83e13c96e91130df1ab9ab67fab238da174

--- index.js
+++ index.js
@@ -10,7 +10,14 @@ const tutorials = [
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
 ];
-
+function titleCasingArray(str){
+  //str = str.toLowerCase();
+  str = str.split(" ");
+  for(let i = 0; i < str.length; i++){
+    str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
+  }
+  return str.join(" ")
+}
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(titleCasingArray)
 }
